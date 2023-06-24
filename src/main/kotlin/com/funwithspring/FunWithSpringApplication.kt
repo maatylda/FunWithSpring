dd02df430dfd64135daf9822df740e0c8782ee37
Matylda <matylda.badowska@gmail.com>
2023-06-24 15:21:05 +0200

    Initial commit

---
+++ src/main/kotlin/com/funwithspring/FunWithSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.funwithspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunWithSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<FunWithSpringApplication>(*args)
+}
